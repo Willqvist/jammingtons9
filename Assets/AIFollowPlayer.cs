55049489132318e6c49f52b7e73bd8ed3dcdba61
Mojko <mojkoow@gmail.com>
2020-08-14 18:55:52 +0200

    added ai jumping

--- Assets/AIFollowPlayer.cs
+++ Assets/AIFollowPlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class AIFollowPlayer : MonoBehaviour
 {
     public Animator animator;
+    public float jumpForce;
 
     private DirectionHolder directionHolder;
     private Rigidbody2D rb;
@@ -23,6 +24,27 @@ public class AIFollowPlayer : MonoBehaviour
     private void Update()
     {
         this.rb.velocity = new Vector2(5 * this.directionHolder.Direction.x, this.rb.velocity.y);
+
+        var hits = Physics2D.RaycastAll(this.transform.position, Vector2.up);
+
+        foreach(var hit in hits)
+        {
+            if(hit.transform.CompareTag("Player"))
+            {
+                Jump();
+            }
+        }
+
+        if(Random.Range(0, (10000f * 1.2f)) < 10)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = movement;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
