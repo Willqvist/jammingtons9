57508f8a37661770e0318526dcd7bccc28908a85
Willqvist <willqvist@gmail.com>
2020-08-14 19:02:00 +0200

    things

---
+++ Assets/SceneFader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+
+    public CanvasGroup group;
+    public Volume volume;
+    public float speed;
+
+    private bool fading = false;
+
+    private Bloom bloom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!volume.profile.TryGet<Bloom>(out bloom))
+        {
+            Debug.Log("CANT GET BLOOM!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fading)
+        {
+            group.alpha = Mathf.Lerp(group.alpha, 1, Time.deltaTime * speed);
+            bloom.intensity.value = Mathf.Lerp(bloom.intensity.value, 12f, Time.deltaTime * speed);
+            if (group.alpha > 0.98)
+            {
+                bloom.intensity.value = 0;
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void Fade()
+    {
+        fading = true;
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/Dialogues/StartTalkDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTalkDialogue : Dialogue
+{
+
+    public SceneFader fader;
+    
+    protected override void onStart()
+    {
+        base.onStart();
+        setupDialogue("none", () => { });
+    }
+
+    protected override async void dialogue()
+    {
+        await this.wait(2);
+        await this.showContinue("Child", "Dad! Dad! Tell me about that story again back in Mexico 1987!");
+        await this.showContinue("Dad", "Okay Son, but it was along time ago so i will have to try hard to bring back my memories.");
+        await this.showContinue("Dad", "It first started when...");
+        end();
+        fader.Fade();
+    }
+}
